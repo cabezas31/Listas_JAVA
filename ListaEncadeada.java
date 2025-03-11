a9ac1a893b3d701855e0cbf500d32a3ff42b05d5
Rodrigo <cabezas31@edu.unifor.br>
2025-03-11 17:31:14 -0300

    Método inserirNoInicio da ListaEncadeada

---
+++ ListaEncadeada.java
@@ -0,0 +1,24 @@
+package Listas_JAVA;
+
+public class ListaEncadeada {
+    private No cabeca;
+    private No cauda;
+    private int tamanho;
+
+    public ListaEncadeada() {
+        this.cabeca = null;
+        this.cauda = null;
+        this.tamanho = 0;
+    }
+
+    public void inserirNoInicio(int elemento) {
+        No novoNo = new No(elemento);
+        if(tamanho == 0) {
+            cabeca = cauda = novoNo;
+        } else {
+            novoNo.setProximo(cabeca);
+            cabeca = novoNo;
+        }
+        tamanho++;
+    }
+}
