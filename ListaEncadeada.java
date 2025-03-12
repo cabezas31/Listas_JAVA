fbc92362cc57222d7ad05e0695584a9c6543a90c
Rodrigo <cabezas31@edu.unifor.br>
2025-03-12 19:21:41 -0300

    método inserir

--- ListaEncadeada.java
+++ ListaEncadeada.java
@@ -13,7 +13,7 @@ public class ListaEncadeada {
 
     public void inserirNoInicio(int elemento) {
         No novoNo = new No(elemento);
-        if(tamanho == 0) {
+        if(estarVazio()) {
             cabeca = cauda = novoNo;
         } else {
             novoNo.setProximo(cabeca);
@@ -24,12 +24,37 @@ public class ListaEncadeada {
 
     public void inserirNoFim(int elemento) {
         No novoNo = new No(elemento);
-        if(tamanho == 0) {
+        if(estarVazio()) {
             cabeca = cauda = novoNo;
         } else {
             cauda.setProximo(novoNo);
             cauda = novoNo;
         }
         tamanho++;
+    }
+
+    public boolean estarVazio() {
+        return tamanho == 0;
+    }
+
+    public void inserir(int posicao, int elemento) {
+        No novoNo = new No(elemento);
+        if(posicao < 0 || posicao > tamanho) {
+            throw new IndexOutOfBoundsException("Posição inválida");
+        }
+        if(posicao == 0) {
+            inserirNoInicio(elemento);
+        } else if(posicao == tamanho){
+            inserirNoFim(elemento);
+        } else {
+            No atual = cabeca;
+            for(int i = 0; i > posicao; i--) {
+                    atual = atual.getProximo();
+            }
+            novoNo.setProximo(atual.getProximo());
+            atual.setProximo(novoNo);
+            tamanho++;   
+        }
+
     }
 }
